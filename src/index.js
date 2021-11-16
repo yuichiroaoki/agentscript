91ba5f039e10334865b00527eafc0e8a4f5967d4
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-16 15:09:26 +0900

    duplicate export

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ import Patch from "./Patch";
 import Turtles from "./Turtles";
 import Model from "./Model";
 
-export default { AgentArray, AgentSet, AgentList, Animator, World, PatchesView, TurtlesView, Color, Patch, Turtles, TurtlesView, Model };
+export { AgentArray, AgentSet, AgentList, Animator, World, PatchesView, Color, Patch, Turtles, TurtlesView, Model };

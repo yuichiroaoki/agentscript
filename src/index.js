9df0672512f6fbce837e5a9f53c89f993e86d5a7
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-16 11:57:03 +0900

    export src

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import AgentArray from "./AgentArray";
+import AgentList from "./AgentList";
+import AgentSet from "./AgentSet";
+import Animator from "./Animator";
+import World from "./World";
+import PatchesView from "./PatchesView";
+import TurtlesView from "./TurtlesView";
+import Color from "./Color";
+import Patch from "./Patch";
+import Turtles from "./Turtles";
+import Model from "./Model";
+
+export default { AgentArray, AgentSet, AgentList, Animator, World, PatchesView, TurtlesView, Color, Patch, Turtles, TurtlesView, Model };

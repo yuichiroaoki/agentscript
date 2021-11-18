6161b34d95f605ff0a65adf64be8c781a64099f0
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-18 11:12:52 +0900

    fix rollup conf

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,12 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import copy from "rollup-plugin-copy";
 import { babel } from "@rollup/plugin-babel";
 
 const packageJson = require("./package.json");
 
 export default {
-  input: "src/index.js",
+  input: "src/index.ts",
   output: [
     {
       file: packageJson.main,
@@ -27,19 +26,5 @@ export default {
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     babel({ babelHelpers: "bundled" }),
-    copy({
-      targets: [
-        {
-          src: "src/variables.scss",
-          dest: "build",
-          rename: "variables.scss",
-        },
-        {
-          src: "src/typography.scss",
-          dest: "build",
-          rename: "typography.scss",
-        },
-      ],
-    }),
   ],
 };

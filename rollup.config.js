d37918a0143414c5b884c609793b64b4d1db73f3
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-18 11:06:12 +0900

    rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-// import typescript from "rollup-plugin-typescript2";
+import typescript from "rollup-plugin-typescript2";
 import copy from "rollup-plugin-copy";
 import { babel } from "@rollup/plugin-babel";
 
@@ -25,7 +25,7 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    // typescript({ useTsconfigDeclarationDir: true }),
+    typescript({ useTsconfigDeclarationDir: true }),
     babel({ babelHelpers: "bundled" }),
     copy({
       targets: [

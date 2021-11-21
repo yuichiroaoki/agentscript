320bfb9ed4f28df78483500c29c6eb6e078d5d30
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-21 21:57:11 +0900

    delete .js

--- src/AS.ts
+++ src/AS.ts
@@ -2,51 +2,51 @@
 // I.e. 2D/3D Models and 2D/3D views
 // It is generally only used by Rollup for bundling.
 
-export * as util from "./utils.js";
-export * as steg from "./steg.js";
+export * as util from "./utils";
+export * as steg from "./steg";
 
 // Models:
-export { default as AgentArray } from "./AgentArray.js";
-export { default as AgentList } from "./AgentList.js";
-export { default as AgentSet } from "./AgentSet.js";
-export { default as DataSet } from "./DataSet.js";
-export { default as Link } from "./Link.js";
-export { default as Links } from "./Links.js";
-export { default as Model } from "./Model.js";
-export { default as Model2D } from "./Model2D.js";
-export { default as Model3D } from "./Model3D.js";
-export { Object3D } from "./vendor/Object3D.js";
-export { default as Patch } from "./Patch.js";
-export { default as Patches } from "./Patches.js";
-export { default as PatchesView } from "./PatchesView.js";
-export { default as RGBDataSet } from "./RGBDataSet.js";
-export * as RGBADataSet from "./RGBADataSet.js";
-export * as TileDataSet from "./TileDataSet.js";
-export { default as Turtle2D } from "./Turtle2D.js";
-export { default as Turtle3D } from "./Turtle3D.js";
-export { default as Turtles } from "./Turtles.js";
-export { default as World } from "./World.js";
+export { default as AgentArray } from "./AgentArray";
+export { default as AgentList } from "./AgentList";
+export { default as AgentSet } from "./AgentSet";
+export { default as DataSet } from "./DataSet";
+export { default as Link } from "./Link";
+export { default as Links } from "./Links";
+export { default as Model } from "./Model";
+export { default as Model2D } from "./Model2D";
+export { default as Model3D } from "./Model3D";
+export { Object3D } from "./vendor/Object3D";
+export { default as Patch } from "./Patch";
+export { default as Patches } from "./Patches";
+export { default as PatchesView } from "./PatchesView";
+export { default as RGBDataSet } from "./RGBDataSet";
+export * as RGBADataSet from "./RGBADataSet";
+export * as TileDataSet from "./TileDataSet";
+export { default as Turtle2D } from "./Turtle2D";
+export { default as Turtle3D } from "./Turtle3D";
+export { default as Turtles } from "./Turtles";
+export { default as World } from "./World";
 
 // Views
-export { default as Color } from "./Color.js";
-export { default as ColorMap } from "./ColorMap.js";
-export { default as Shapes } from "./Shapes.js";
-export { default as SpriteSheet } from "./SpriteSheet.js";
-export { default as TurtlesView } from "./TurtlesView.js";
-export { default as TwoDraw } from "./TwoDraw.js";
-export { default as TwoView } from "./TwoView.js";
+export { default as Color } from "./Color";
+export { default as ColorMap } from "./ColorMap";
+export { default as Shapes } from "./Shapes";
+export { default as SpriteSheet } from "./SpriteSheet";
+export { default as TurtlesView } from "./TurtlesView";
+export { default as TwoDraw } from "./TwoDraw";
+export { default as TwoView } from "./TwoView";
 
 // ThreeJS Views, excluded from smaller rollups
-export { default as ThreeDraw } from "./ThreeDraw.js";
-export { default as ThreeMeshes } from "./ThreeMeshes.js";
-export { default as ThreeView } from "./ThreeView.js";
+export { default as ThreeDraw } from "./ThreeDraw";
+export { default as ThreeMeshes } from "./ThreeMeshes";
+export { default as ThreeView } from "./ThreeView";
 
 // Controls
-export { default as Animator } from "./Animator.js";
-export { default as Evented } from "./Evented.js";
-export { default as Mouse } from "./Mouse.js";
+export { default as Animator } from "./Animator";
+export { default as Evented } from "./Evented";
+export { default as Mouse } from "./Mouse";
 
 // GIS
-export * as gis from "./gis.js";
-export * as geojson from "./geojson.js";
-export { default as GeoWorld } from "./GeoWorld.js";
+export * as gis from "./gis";
+export * as geojson from "./geojson";
+export { default as GeoWorld } from "./GeoWorld";

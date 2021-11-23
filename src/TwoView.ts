3c09abd49a5e937eb1ba01d92533d87f48998e0b
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-23 10:38:37 +0900

    two view type

--- src/TwoView.ts
+++ src/TwoView.ts
@@ -1,10 +1,25 @@
-import * as util from "./utils.js";
-import World from "./World.js";
-import PatchesView from "./PatchesView.js";
-import TurtlesView from "./TurtlesView.js";
+import * as util from "./utils";
+import World from "./World";
+import PatchesView from "./PatchesView";
+import TurtlesView from "./TurtlesView";
+import { IDrawOptions } from "./TwoDraw";
+
+export interface IViewOptions {
+  div?: any;
+  useSprites?: boolean;
+  drawOptions?: IDrawOptions;
+  patchSize?: number;
+  width?: number;
+}
 
 export default class TwoView {
-  static defaultOptions() {
+  div: any;
+  ctx: any;
+  world: World;
+  patchesView: PatchesView;
+  turtlesView: TurtlesView;
+  ticks: number;
+  static defaultOptions(): IViewOptions {
     return {
       div: document.body,
       useSprites: false,
@@ -16,7 +31,7 @@ export default class TwoView {
   // ======================
 
   // Note: world can be a model, who'd model.world will be used
-  constructor(world, options = {}) {
+  constructor(world: any, options: IViewOptions = {}) {
     // options: override defaults, assign to this
     // Object.assign(this, TwoView.defaultOptions(), options)
     options = Object.assign(TwoView.defaultOptions(), options);
@@ -97,7 +112,7 @@ export default class TwoView {
   // Clear the view.canvas.
   // If no color, or 'transparent', make transparent via clearCtx.
   // If cssColor.css (typedColor) use it, else cssColor
-  clear(cssColor) {
+  clear(cssColor = undefined) {
     util.clearCtx(this.ctx, cssColor);
 
     // REMIND: Clear the pixels?
@@ -125,7 +140,7 @@ export default class TwoView {
   }
 
   // If no data, redraw with existing patchesView cache
-  drawPatches(data, pixelFcn) {
+  drawPatches(data?: any, pixelFcn?: any) {
     if (data != null) {
       this.patchesView.setPixels(data, pixelFcn);
     }

7c94e1d2e0ee6838e2ebb70b466de9cd8fefb9c2
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-27 20:17:48 +0900

    export turtle, colormap

--- src/index.js
+++ src/index.js
@@ -6,12 +6,17 @@ import World from "./World";
 import PatchesView from "./PatchesView";
 import TurtlesView from "./TurtlesView";
 import Color from "./Color";
+import ColorMap from "./ColorMap";
 import Patch from "./Patch";
+import Patches from "./Patches";
 import Turtles from "./Turtles";
+import Turtle from "./Turtle2D";
 import Model from "./Model";
 import TwoDraw from "./TwoDraw";
+import TwoView from "./TwoView";
 import Link from "./Link";
 import Links from "./Links";
+import * as utils from "./utils";
 
 export {
   AgentArray,
@@ -21,11 +26,16 @@ export {
   World,
   PatchesView,
   Color,
+  ColorMap,
   Patch,
+  Patches,
   Turtles,
+  Turtle,
   TurtlesView,
   Model,
   TwoDraw,
+  TwoView,
   Link,
   Links,
+  utils,
 };
